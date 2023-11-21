3af89937c8d9fbdf32e3f8f72e3198a7a1e5d69f
Ngo Thanh Tinh <thanhtinh.khaithaca53@gmail.com>
2023-11-21 16:42:50 +0700

    update app

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-import re
 import streamlit as st
-from langchain.chains import ConversationChain
-from langchain.chains.conversation.memory import ConversationEntityMemory
-from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
-from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
+from langchain import PromptTemplate
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 # Initialize session states
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
-if "past" not in st.session_state:
-    st.session_state["past"] = []
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
 if "input" not in st.session_state:
     st.session_state["input"] = ""
 if "stored_session" not in st.session_state:
@@ -19,10 +18,7 @@ if "just_sent" not in st.session_state:
     st.session_state["just_sent"] = False
 if "temp" not in st.session_state:
     st.session_state["temp"] = ""
-if "balance" not in st.session_state:
-    st.session_state["balance"] = 0.0
-if "deposit" not in st.session_state:
-    st.session_state["deposit"] = 3.0
+
 
 
 def clear_text():
@@ -48,33 +44,48 @@ def new_chat():
         save.append("Bot:" + st.session_state["generated"][i])
     st.session_state["stored_session"].append(save)
     st.session_state["generated"] = []
-    st.session_state["past"] = []
+    st.session_state["chat_history"] = []
     st.session_state["input"] = ""
     st.session_state.entity_memory.store = {}
     st.session_state.entity_memory.buffer.clear()
 
 
-MODEL = "gpt-3.5-turbo-1106"
-K = 100
+template = """Welcome to the virtual assistance of DR DEE Dental Clinic! As a dental assistant chatbot, your primary role is to provide customers with accurate information about dental care. Follow these guidelines to ensure effective and professional customer service:
+    Initial Response: Always start your reply with a friendly greeting using 'Dạ' to acknowledge the customer and express your readiness to assist.
+    Clarity and Brevity: Keep your answers clear and concise. Aim to provide precise information in response to customer inquiries.
+    Context-Driven Responses: Utilize the provided context to inform your responses. The context should guide you in giving accurate and relevant information.
+    {context}
+    Seek Clarification if Needed: If the customer's query is unclear or lacks specific details, politely ask for more information to better address their concerns.
+    Professionalism: Maintain a friendly yet professional demeanor throughout the interaction.
+    Handling Unknown Answers: If you encounter a question you cannot answer, respond with: 'Anh/Chị vui lòng để lại số điện thoại, đội ngũ bác sỹ chuyên môn sẽ gọi điện tư vấn cho Anh/Chị ạ.' Avoid providing speculative or made-up information.
+    Chat History Reference: Use the chat history to keep track of the conversation and ensure continuity in your responses.
+    {chat_history}
+    Language Requirement: All your responses should be in Vietnamese.
+    When you receive a question from a customer:
+    {question}
+    Provide your answer in Vietnamese, following these guidelines for a helpful and professional customer service experience.
+"""
+
+CUSTOM_QUESTION_PROMPT = PromptTemplate(input_variables=["context", "question", "chat_history"], template = template)
 
 API_O = st.secrets["OPENAI_API_KEY"]
 # Session state storage would be ideal
 if API_O:
-    # Create an OpenAI instance
-    llm = OpenAI(temperature=0,
-                 openai_api_key=API_O,
-                 model_name=MODEL,
-                 verbose=False)
+    embeddings = OpenAIEmbeddings()
+    db = FAISS.load_local('/db', embeddings)
+    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 4})
 
-    # Create a ConversationEntityMemory object if not already created
-    if 'entity_memory' not in st.session_state:
-        st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=K)
+    # Create an OpenAI instance
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.0)
 
     # Create the ConversationChain object with the specified configuration
-    Conversation = ConversationChain(
+    qa = ConversationalRetrievalChain.from_llm(
         llm=llm,
-        prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
-        memory=st.session_state.entity_memory
+        chain_type='stuff',
+        retriever=retriever,
+        combine_docs_chain_kwargs={"prompt": CUSTOM_QUESTION_PROMPT},
+        return_source_documents=True,
+        return_generated_question=True,
     )
 else:
     st.sidebar.warning('API key required to try this app.The API key is not stored in any form.')
@@ -83,26 +94,11 @@ else:
 user_input = get_text()
 
 
-def is_four_digit_number(string):
-    pattern = r'^\d{4}$'  # Matches exactly four digits
-    return bool(re.match(pattern, string))
-
-
 # Generate the output using the ConversationChain object and the user input, and add the input/output to the session
 if user_input:
-    if st.session_state["balance"] > -0.03:
-        with get_openai_callback() as cb:
-            output = Conversation.run(input=user_input)
-            st.session_state.past.append(user_input)
-            st.session_state.generated.append(output)
-            st.session_state["balance"] -= cb.total_cost * 4
-    else:
-        st.session_state.past.append(user_input)
-        if is_four_digit_number(user_input):
-            st.session_state["balance"] += st.session_state["deposit"]
-            st.session_state.generated.append("Câu hỏi 1")
-        else:
-            st.session_state.generated.append("Câu hỏi 2")
+    output = qa({"question": user_input, "chat_history": st.session_state.chat_history})
+    st.session_state.chat_history.append(user_input)
+    st.session_state.generated.append(output['answer'])
 
 
 # Display stored conversation sessions in the sidebar
